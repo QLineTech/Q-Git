1d88e63b579ffd1ced737d011feb6f1795b79c60
Keyvan Arasteh <keyvan.arasteh@live.com>
2026-01-19 16:18:43 +0300

    run-web script.

---
+++ run-web.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create virtual environment if not exists
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Install packages if not installed
+pip install -r requirements.txt
+
+# Run main.py
+python3 web.py
